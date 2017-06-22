43b2008267f382e2b46d6e1c43983855a31af355
maxleiko <max.tricoire@gmail.com>
2017-06-22 15:57:22 +0200

    v5.3.3-beta.4: fix browser entry

--- src/browser/main.js
+++ src/browser/main.js
@@ -1,8 +1,5 @@
 /* globals KevoreeModuleLoader */
-() => {
-	console.log('swag'); // eslint-disable-line
-};
-// const pkg = require('../../package.json');
-// const Ticker = require('./TickerUI');
-//
-// KevoreeModuleLoader.register(pkg.name, pkg.version, Ticker);
+const pkg = require('../../package.json');
+const Ticker = require('./TickerUI');
+
+KevoreeModuleLoader.register(pkg.name, pkg.version, Ticker);
